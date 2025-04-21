b3bfeb02c36f304f3ea68079f821e7a9ebef4734
adrian <adrianmoisesfls@gmail.com>
2025-04-21 14:26:49 -0400

    first commit

---
+++ src/lib/data.ts
@@ -0,0 +1,120 @@
+export const personalInfo = {
+  name: "Adrian Flores",
+  location: "Santiago, Chile",
+  email: "adrianmoisesfls@gmail.com",
+  github: "https://github.com/adrianmfm",
+  linkedin: "https://www.linkedin.com/in/adrianmfloresss/",
+};
+
+export const workExperience = [
+  {
+    company: "Tooxs",
+    location: "Santiago, Chile",
+    position: "Software Engineer",
+    period: "Nov 2024 - Mar 2025",
+    achievements: [
+      "Participé en el desarrollo de una plataforma educativa orientada a profesores, que integraba inteligencia artificial basada en el currículo nacional para facilitar la gestión de clases mediante componentes como lista de estudiantes, libro de clases y otros recursos. Dentro del equipo, me enfoqué en la implementación de interfaces interactivas y responsivas, asegurando una experiencia de usuario óptima en distintos dispositivos. Convertí diseños desde Figma a interfaces web accesibles y de alta fidelidad. También colaboré en la creación de servicios en Angular para consumir APIs de forma eficiente, utilizando RxJS para manejar flujos de datos de manera reactiva. Contribuí al desarrollo y mejora de componentes reutilizables con Angular y PrimeNG, lo que permitió optimizar tiempos de desarrollo y mantener un código más limpio. Además, participé en el diseño y desarrollo de la landing page del proyecto utilizando Astro y React.",
+    ],
+  },
+  {
+    company: "Banco BCI",
+    location: "Santiago, Chile",
+    position: "Frontend developer Marketing - Intership",
+    period: "Mar 2024 - Ago 2024",
+    achievements: [
+      "Trabajé como desarrollador frontend dentro de un equipo de optimización de conversión (CRO), donde me encargaba de implementar experimentos de A/B testing enfocados en mejorar la experiencia del usuario en las plataformas digitales del banco. Utilicé tecnologías como HTML, CSS, JavaScript y Angular para desarrollar interfaces basadas en diseños de Figma, asegurando una correcta visualización y funcionamiento. Aunque el análisis de datos lo realizaba el equipo de CRO, colaboraba estrechamente con ellos para implementar los cambios necesarios según los resultados obtenidos en herramientas como VWO, Google Analytics y Google Tag Manager. Gracias a estas implementaciones, contribuí indirectamente a la mejora en la tasa de conversión de productos digitales del banco..",
+    ],
+  },
+  {
+    company: "Genera CC",
+    location: "Santiago, Chile",
+    position: "Analista de desarrollo",
+    period: "Ene 2023 - Ago 2023",
+    achievements: [
+      "Desarrollé el módulo de administración para un call center utilizando React, enfocándome en la construcción de interfaces funcionales. Además de mi rol principal como desarrollador frontend, brindé apoyo en el backend mediante el desarrollo de microservicios en Java con Spring Boot. También participé activamente en tareas relacionadas con bases de datos, realizando limpieza, preparación y carga de datos para campañas en PostgreSQL y gestionando bases en SQL Server.",
+    ],
+  },
+];
+
+export const education = [
+  {
+    institution: "Duoc UC",
+    location: "Santiago, Chile",
+    degree: "Analista Programador",
+    period: "Ene 2022 - Jun 2024",
+    achievements: [
+      "Participación en proyectos académicos de desarrollo web y análisis de datos",
+    ],
+  },
+  {
+    institution: "Duoc UC",
+    location: "Santiago, Chile",
+    degree: "Ingeniero en Informática",
+    period: "2022 - 2025 (vespertino)",
+    achievements: [
+      "Estudios enfocados en desarrollo de software, gestión de proyectos y análisis de datos",
+      "Participación en proyectos académicos de desarrollo de sistemas y arquitectura en la nube"
+    ],
+  },
+];
+
+
+export const skills = {
+  programmingLanguages: [
+    "TypeScript",
+    "JavaScript",
+    "Python",
+    "Java",
+  ],
+  frontendDevelopment: [
+    "Nextjs",
+    "Reactjs",
+    "Angular",
+    "Tailwind CSS",
+    "Bootstrap",
+    "Astro",
+    "HTML",
+    "CSS",
+  ],
+  dataAnalysis: [
+    "Python",
+    "Pandas",
+    "NumPy",
+    "Matplotlib",
+    "Power BI",
+    "Tableau",
+    "Excel",
+    "Google Analytics",
+    "SQL",
+  ],
+  backendDevelopment: ["Nodejs", "Expressjs", "Spring Boot"],
+  databaseAndStorage: ["PostgreSQL", "SQL Server"],
+};
+
+export const projects = [
+  {
+    title: "Mi pequeña Joya",
+    github: "https://github.com/adrianmfm/joya-front",
+    description: [
+      "Tienda online de joyería.",
+      "Backend desarrollado con Spring Boot y SQLite como base de datos.",
+      "Frontend desarrollado con React y Bootstrap para diseño responsivo.",
+      "Funcionalidades principales incluyen visualización de productos, agregar al carrito y realizar compras.",
+      "Panel de administración accesible en la ruta /admin para gestionar productos (agregar, editar, eliminar).",
+      "Swagger integrado para documentación de la API."
+    ]
+  },
+  {
+    title: "Zofia - Plataforma Educativa",
+    github: "https://zofia.app",
+    description: [
+        "Landing page para la plataforma educativa Zofia.",
+        "Diseño limpio y profesional con enfoque en la accesibilidad.",
+        "Incluye información sobre los cursos, características y beneficios de la plataforma.",
+        "Formulario de contacto para que los usuarios se registren o pidan más información.",
+        "Responsive design, optimizado para visualización en dispositivos móviles y de escritorio.",
+        "Integración con enlaces a redes sociales y llamada a la acción clara para registrarse."
+        ],
+  },
+];
+
