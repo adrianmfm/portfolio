1fffbe693b28e7bc38411ef9ebe39a95bf26944a
adrian <adrianmoisesfls@gmail.com>
2025-04-21 15:08:45 -0400

    cname

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://github.com/adrianmfm/portfolio.git',
-  base: '/portfolio/',
+  site: 'portafoliowebadrian.cl',
   devToolbar: {
     enabled: false
   },
